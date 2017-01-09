2e13446037e2ca326ce6dc0dd93992f403f07b2d
Sharath Chandrashekhara <sc296@buffalo.edu>
2017-01-09 17:50:34 -0500

    Typos and minor edits

--- assets/js/beer.js
+++ assets/js/beer.js
@@ -1,20 +1,45 @@
 function loadXMLRecipe(recipe) {
 	var recipe_xml_file = 'assets/recipes/' + recipe + '.xml';
-	var recipe_table = recipe + '_table';
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			displayRecipe(this, recipe_table);
+			displayRecipe(this, recipe);
 		}
 	};
 	xmlhttp.open("GET", recipe_xml_file, true);
 	xmlhttp.send();
 }
 
-function displayRecipe(xml, recipe_table) {
+function displayRecipe(xml, recipe) {
+	var recipe_div = recipe + '_div';
+	var recipe_table = recipe + '_table';
 	var i;
 	var xmlDoc = xml.responseXML;
+
+	var rec_node = xmlDoc.getElementsByTagName("RECIPE")[0];
+	var name =
+		rec_node.getElementsByTagName("NAME")[0].childNodes[0].nodeValue;
+	var summary = "<\p>Name: " + name + "<br>;
+	var size =
+		rec_node.getElementsByTagName("BATCH_SIZE")[0].childNodes[0].nodeValue;
+	summary += "Size: " + (parseFloat(size) / 3.76).toFixed(2) + "G<br>"
+	var type =
+		rec_node.getElementsByTagName("TYPE")[0].childNodes[0].nodeValue;
+	summary += "Type: " + type + "<br>"
+	var og =
+		rec_node.getElementsByTagName("OG")[0].childNodes[0].nodeValue;
+	summary += "OG: " + og + "<br>"
+	var srm =
+		rec_node.getElementsByTagName("SRM")[0].childNodes[0].nodeValue;
+	summary += "SRM: " + srm + "<br>"
+	var ibu =
+		rec_node.getElementsByTagName("IBU")[0].childNodes[0].nodeValue;
+	summary += "IBU: " + ibu + "<br><\p>"
+
+	document.getElementById(recipe_div).innerHTML = summary;
+
 	var table="<tr><th>Ingridient</th><th>Amount</th></tr>";
+
 	var x =
 		xmlDoc.getElementsByTagName("RECIPE")[0].getElementsByTagName("FERMENTABLES")[0].getElementsByTagName("FERMENTABLE");
 	table +="<th colspan='2'>Fermentables</th>"
@@ -32,9 +57,11 @@ function displayRecipe(xml, recipe_table) {
 	for (i = 0; i <x.length; i++) { 
 		var amount_str = x[i].getElementsByTagName("AMOUNT")[0].childNodes[0].nodeValue;
 		var amount = (parseFloat(amount_str) * 35.274).toFixed(2) + ' oz';
+		var time = x[i].getElementsByTagName("TIME")[0].childNodes[0].nodeValue;
+
 		table += "<tr><td>" + 
 			x[i].getElementsByTagName("NAME")[0].childNodes[0].nodeValue +
-			"</td><td>" + amount +
+			"</td><td>" + amount + " (" + time + "mins)"
 			"</td></tr>";
 	}
 
